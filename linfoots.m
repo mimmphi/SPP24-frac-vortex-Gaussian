0f18fe3fef3d4854aeda5e0fa6b64513339dc7cc
mimmphi <mmmendoza31@up.edu.ph>
2024-03-11 00:50:59 +0800

    Organized files
    Renamed a folder (Feb 20 data - raw frac l), deleted linfoots, updated phase_holes.mlx
    
    [note] "replace" = deleted from - moved out of - Github

--- linfoots.m
+++
@@ -1,7 +0,0 @@
-function [Q,T,F] = linfoots(I_o,I_r)
-%I_o: original image
-%I_r: reconstructed image
-Q = sum(sum(I_o.*I_r))/sum(sum(I_o.*I_o));
-T = sum(sum(I_r.*I_r))/sum(sum(I_o.*I_o));
-F = 2*Q - T;
-end
